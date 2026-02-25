c75d5fc6af9d7d19bc1de0dd4e484589d3d7a5ec
Elizabeth Ronan <eronan@uchicago.edu>
2026-02-24 21:20:30 -0600

    Updated app.py script

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, jsonify
+import psycopg2
+import boto3
+import json, subprocess
 
-app = Flask(__name__)
+def get_secret():
+    # Extract secret from AWS
+    secret = subprocess.check_output(
+        ['bash',
+        '-lc',
+        "aws secretsmanager get-secret-value --secret-id 'arn:aws:secretsmanager:us-east-1:834922934112:secret:rds!db-9e259e04-e679-4537-b0f5-70289dc2fde7-2G57H5' --query SecretString --output text"
+        ]
+    ).decode(); password = json.loads(secret)['password']
 
-@app.route('/')
-def hello():
-    return 'Hello from front page!'
+    return secret, password
 
-@app.route('items')
-def list_items():
-    return 'Listing items from amazon db'
+def get_conn(password):
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host='database-ge.cspks6qqgm4n.us-east-1.rds.amazonaws.com',
+            port=5432,
+            database='postgres',
+            user='postgres',
+            password=password,
+            sslmode='verify-full',
+        sslrootcert='/Users/elizabethronan/global-bundle.pem'
+        )
+        cur = conn.cursor()
+        cur.execute('SELECT version();')
+        print(cur.fetchone()[0])
+        cur.close()
+    except Exception as e:
+        print(f"Database error: {e}")
+        raise
+    finally:
+        if conn:
+            print('Connection successful!')
+            conn.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    secret, password = get_secret()
+    get_conn(password)
